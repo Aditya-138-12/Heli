a1a7fcea374aa5f694278ab838ad9ca29685c52a
ADITYA SAROHA <aditya@localhost.localdomain>
2024-07-12 23:04:42 +0530

    Added All the Files, Heli

---
+++ site/main.js
@@ -0,0 +1,35 @@
+document.getElementById("signUpBtn").addEventListener("click", () => {
+
+	userEmail = document.getElementById("alternateSignUpDivEmail").value;
+	userPassword = document.getElementById("alternateSignUpDivPassword").value;
+	
+	//console.log(userEmail, userPassword);
+	
+	
+
+});
+
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyB1AcQ8Gx0IBA57bIMv3O51kojxUlsZJEw",
+authDomain: "[heli-8370a.firebaseapp.com](http://heli-8370a.firebaseapp.com/)",
+projectId: "heli-8370a",
+storageBucket: "[heli-8370a.appspot.com](http://heli-8370a.appspot.com/)",
+messagingSenderId: "454703904163",
+appId: "1:454703904163:web:28e75fdc0323889c009616",
+measurementId: "G-J3KZ1KD2JX"
+};
+
+// Initialize Firebase
+const app =  initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(app);
+*/
